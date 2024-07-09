fb4c96ab38a9bf66c155d06b6095bce2c7719683
Olabisitee <oladepoolabisitee2001@gmail.com>
2024-07-09 06:56:48 -0700

    modify changes

--- src/components/SuccessfulPay.jsx
+++ src/components/SuccessfulPay.jsx
@@ -3,7 +3,7 @@ import "./SuccessfulPay.css";
 const SuccessfulPay = () => {
   return (
     <div className="successful-page">
-      <img href="\images\IMG_7038 1.png" alt="icon" />
+      <img href="#" alt="icon" />
       <h3>Payment Successful</h3>
       <p>You have successfully place your order. Thank you.</p>
     </div>
